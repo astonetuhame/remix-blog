6921a95ba7e57411644bbd9cd78726406f5a816d
Astone Tuhame <astonetuhame@gmail.com>
2021-12-17 02:14:00 +0300

    Create index.jsx

---
+++ app/routes/posts/index.jsx
@@ -0,0 +1,39 @@
+import { useLoaderData, Link } from 'remix'
+
+export const loader = () => {
+    const data = {
+        posts: [
+            { id: 1, title: 'Post 1', body: 'Test post' },
+            { id: 2, title: 'Post 2', body: 'Test post' },
+            { id: 3, title: 'Post 3', body: 'Test post' }
+        ]
+    }
+    return data
+}
+
+function PostItems() {
+    const { posts } = useLoaderData()
+    return (
+        <>
+            <div className="page-header">
+                <h1>Posts</h1>
+                <Link to="/posts/new" className='btn'>
+                    New Post
+                </Link>
+            </div>
+            
+            <ul className="posts-list">
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link to={post.id}>
+                            <h3>{post.title}</h3>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+        </>
+    )
+}
+
+export default PostItems
