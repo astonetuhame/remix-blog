fb4f9bf606e5eede1ca49b8415250968152037a5
Astone Tuhame <astonetuhame@gmail.com>
2021-12-17 02:41:46 +0300

    Update new.jsx

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,4 +1,22 @@
-import { Link } from "remix"
+import { Link, redirect } from "remix"
+
+export const action = ({request}) => {
+
+    const form = await request.formData()
+
+    const title = form.get('title')
+    const body = form.get('body')
+
+    const fields = {title, body}
+
+    //submit to database
+
+
+
+    return redirect('/posts')
+}
+
+
 
 function NewPost() {
     return (
@@ -6,7 +24,22 @@ function NewPost() {
             <div className="page-header">
                 <h1>New Post</h1>
                 <Link to="/posts" className="btn btn reverse">
+                Back
                 </Link>
+            </div>
+
+            <div className="page-content">
+                <form method="post">
+                    <div className="form-control">
+                        <label htmlFor="title">Title</label>
+                        <input type="text" name="title" id="title" />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="body">Post Body</label>
+                        <textarea name="body" id="body" />
+                        <button className="btn btn-block" type="submit"></button>
+                    </div>
+                </form>
             </div>
         </>
     )
