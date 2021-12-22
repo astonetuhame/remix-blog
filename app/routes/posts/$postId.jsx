25fa80a82f38135f972dfcd46aa20839e1460415
Astone Tuhame <astonetuhame@gmail.com>
2021-12-22 16:54:42 +0300

    Enable user to delete their posts only

--- app/routes/posts/$postId.jsx
+++ app/routes/posts/$postId.jsx
@@ -1,55 +1,57 @@
-import { useLoaderData, Link, redirect } from 'remix'
-import { db } from '~/utils/db.server'
+import { useLoaderData, Link, redirect } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 
-export const loader = async ({ params }) => {
-    const post = await db.post.findUnique({
-        where: { id: params.postId }
-    })
+export const loader = async ({ request, params }) => {
+  const user = await getUser(request);
+  const post = await db.post.findUnique({
+    where: { id: params.postId },
+  });
 
-    if (!post) throw new Error('Post not found')
-    const data = { post }
+  if (!post) throw new Error("Post not found");
+  const data = { post, user };
 
-    return data
-}
+  return data;
+};
 
 export const action = async ({ request, params }) => {
-    const form = await request.formData()
-    if (form.get('_method') === 'delete') {
-        const post = await db.post.findUnique({
-            where: { id: params.postId }
-        })
+  const form = await request.formData();
+  if (form.get("_method") === "delete") {
+    const user = await getUser(request);
+    const post = await db.post.findUnique({
+      where: { id: params.postId },
+    });
 
-        if (!post) throw new Error('Post not found')
-        await db.post.delete({ where: { id: params.postId } })
-
-        return redirect('/posts')
+    if (!post) throw new Error("Post not found");
+    if (user && post.userId === user.id) {
+      await db.post.delete({ where: { id: params.postId } });
     }
-}
 
-
+    return redirect("/posts");
+  }
+};
 
 function Post() {
-
-    const { post } = useLoaderData()
-    return (
-        <div>
-            <div className="page-header">
-                <h1>{post.title}</h1>
-                <Link to='/posts' className="btn btn-reverse">
-                    Back
-                </Link>
-            </div>
-            <div className="page-content">
-                {post.body}
-            </div>
-            <div className="page-footer">
-                <form method="POST">
-                    <input type="hidden" name="_method" value="delete" />
-                    <button className="btn btn-delete">Delete</button>
-                </form>
-            </div>
-        </div>
-    )
+  const { post, user } = useLoaderData();
+  return (
+    <div>
+      <div className="page-header">
+        <h1>{post.title}</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+      <div className="page-content">{post.body}</div>
+      <div className="page-footer">
+        {user.id === post.userId && (
+          <form method="POST">
+            <input type="hidden" name="_method" value="delete" />
+            <button className="btn btn-delete">Delete</button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
