7a1aab8de8518d4572c0091eca13cc095f71edba
Astone Tuhame <astonetuhame@gmail.com>
2021-12-22 15:59:06 +0300

    Create user model and seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,40 +1,46 @@
-const { PrismaClient } = require('@prisma/client')
-const db  = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-
-async function seed(){
-    await Promise.all(
-        getPosts().map(post =>{
-            return db.post.create({ data: post})
-        })
-    )
+async function seed() {
+  const john = await prisma.user.create({
+    data: {
+      username: "john",
+      // Hash for password - twixrox
+      passwordHash:
+        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+    },
+  });
+  await Promise.all(
+    getPosts().map((post) => {
+      const data = { userId: john.id, ...post };
+      return prisma.post.create({ data });
+    })
+  );
 }
 
-seed()
+seed();
 
 function getPosts() {
-    return [
-        {
-            title: 'PHP',
-            body: 'A body about Php'
-        },
-        {
-            title: 'Javascript',
-            body: 'A body about Javascript'
-        },
-        {
-            title: 'Python',
-            body: 'A body about python'
-        },
-        {
-            title: 'Typescript',
-            body: 'A body about typescript'
-        },
-        {
-            title: 'Nodejs',
-            body: 'A body about nodejs'
-        }
-    ]
+  return [
+    {
+      title: "PHP",
+      body: "A body about Php",
+    },
+    {
+      title: "Javascript",
+      body: "A body about Javascript",
+    },
+    {
+      title: "Python",
+      body: "A body about python",
+    },
+    {
+      title: "Typescript",
+      body: "A body about typescript",
+    },
+    {
+      title: "Nodejs",
+      body: "A body about nodejs",
+    },
+  ];
 }
-
-
